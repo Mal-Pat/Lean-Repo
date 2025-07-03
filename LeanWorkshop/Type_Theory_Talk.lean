3b2d08c32f9f9ba99d45dd23ba74c00eb58083f9
Malhar Patel <malhar30000@gmail.com>
2025-07-03 12:10:34 +0000

    code to introduce Lean and basic Type Theory

---
+++ LeanWorkshop/Type_Theory_Talk.lean
@@ -0,0 +1,418 @@
+import Mathlib
+
+
+
+-- Everything has a type
+#check 2
+#check "math club"
+
+
+
+-- Define functions
+def f1 (n : Nat) : Nat :=
+  5*n
+
+#check f1
+#eval f1 2
+
+
+
+-- Using lambda abstraction
+#check (fun (x : Nat) => 5*x)
+#eval (fun (x : Nat) => 5*x) 2
+
+
+
+-- More complicated functions
+def f (n : Nat) : String := toString n
+def g (s : String) : Bool := s.length > 0
+
+#eval f 2
+#eval g "x"
+#check (fun (α β γ : Type) (u : β → γ) (v : α → β) (x : α) => u (v x)) Nat String Bool g f 0
+
+
+
+-- List α is dependent
+#check List
+#check List Nat
+
+def cons (α : Type) (a : α) (as : List α) : List α :=
+  List.cons a as
+
+#check cons Nat
+#check cons Bool
+#check cons
+
+
+
+-- Swap in Lean
+def swap {A B C : Type} (g : A → B → C) : B → A → C :=
+  fun (b : B) => (fun (a : A) => g a b)
+
+def g2 (a : Nat) (b : Nat) : Nat × Nat :=
+  (a,b)
+
+#check g2
+#eval g2 3 5
+
+#check swap
+#check swap g2
+#check swap g2 5
+#check swap g2 5 3
+#eval swap g2 5 3
+
+
+
+-- Prod
+def a : Nat × Nat := (3,5)
+#check Prod.fst
+#eval Prod.fst a
+#check Prod.snd
+#eval Prod.snd a
+
+#check Prod.rec
+
+def first (x : Nat × Nat) : Nat := Prod.rec (fun (a : Nat) => (fun (b : Nat) => a)) x
+#eval first (3,5)
+
+
+
+-- Bool
+#check Bool.rec
+
+
+
+-- Nat
+#check Nat.rec
+
+
+
+-- And
+variable (p q : Prop)
+
+example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
+
+#check fun (hp : p) (hq : q) => And.intro hp hq
+
+
+
+-- Or
+variable (p q r : Prop)
+
+def disjunc (h : p ∨ q) : q ∨ p :=
+  Or.elim h
+    (fun hp : p =>
+      show q ∨ p from Or.intro_right q hp)
+    (fun hq : q =>
+      show q ∨ p from Or.intro_left p hq)
+
+#print disjunc
+
+
+
+-- Neg
+variable (p q : Prop)
+
+example (hpq : p → q) (hnq : ¬q) : ¬p :=
+  fun hp : p =>
+  show False from hnq (hpq hp)
+
+
+
+-- Equiv
+variable (p q : Prop)
+
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+  Iff.intro
+    (fun h : p ∧ q =>
+     show q ∧ p from And.intro (And.right h) (And.left h))
+    (fun h : q ∧ p =>
+     show p ∧ q from And.intro (And.right h) (And.left h))
+
+#check and_swap p q    -- p ∧ q ↔ q ∧ p
+
+variable (h : p ∧ q)
+example : q ∧ p := Iff.mp (and_swap p q) h
+
+
+-- Classical
+open Classical
+
+variable (p : Prop)
+#check em
+#check Classical.em p
+
+
+theorem dne {p : Prop} (h : ¬¬p) : p :=
+  Or.elim (Classical.em p)
+    (fun hp : p => hp)
+    (fun hnp : ¬p => absurd hnp h)
+
+
+
+-- For all
+example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) → ∀ y : α, p y :=
+  fun h : ∀ x : α, p x ∧ q x =>
+  fun y : α =>
+  show p y from (h y).left
+
+
+
+-- There exists
+example : ∃ x : Nat, x > 0 :=
+  have h : 1 > 0 := Nat.zero_lt_succ 0
+  Exists.intro 1 h
+
+example (x : Nat) (h : x > 0) : ∃ y, y < x :=
+  Exists.intro 0 h
+
+example (x y z : Nat) (hxy : x < y) (hyz : y < z) : ∃ w, x < w ∧ w < z :=
+  Exists.intro y (And.intro hxy hyz)
+
+#check @Exists.intro -- ∀ {α : Sort u_1} {p : α → Prop} (w : α), p w → Exists p
+
+
+
+def is_even (a : Nat) := ∃ b, a = 2 * b
+
+theorem even_plus_even (a b : Nat)(h1 : is_even a) (h2 : is_even b) : is_even (a + b) :=
+  Exists.elim h1 (fun w1 (hw1 : a = 2 * w1) =>
+  Exists.elim h2 (fun w2 (hw2 : b = 2 * w2) =>
+    Exists.intro (w1 + w2)
+      (calc a + b
+        _ = 2 * w1 + 2 * w2 := by rw [hw1, hw2]
+        _ = 2 * (w1 + w2)   := by rw [Nat.mul_add])))
+
+
+
+
+open Classical
+variable (p : α → Prop)
+
+example (h : ¬ ∀ x, ¬ p x) : ∃ x, p x :=
+  byContradiction
+    (fun h1 : ¬ ∃ x, p x =>
+      have h2 : ∀ x, ¬ p x :=
+        fun x =>
+        fun h3 : p x =>
+        have h4 : ∃ x, p x := ⟨x, h3⟩
+        show False from h1 h4
+      show False from h h2)
+
+
+
+open Classical
+
+variable (α : Type) (p q : α → Prop)
+variable (a : α)
+variable (r : Prop)
+
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
+  Iff.intro
+    (fun ⟨a, (h1 : p a ∨ q a)⟩ =>
+      Or.elim h1
+        (fun hpa : p a => Or.inl ⟨a, hpa⟩)
+        (fun hqa : q a => Or.inr ⟨a, hqa⟩))
+    (fun h : (∃ x, p x) ∨ (∃ x, q x) =>
+      Or.elim h
+        (fun ⟨a, hpa⟩ => ⟨a, (Or.inl hpa)⟩)
+        (fun ⟨a, hqa⟩ => ⟨a, (Or.inr hqa)⟩))
+
+example : (∃ x, p x → r) ↔ (∀ x, p x) → r :=
+  Iff.intro
+    (fun ⟨b, (hb : p b → r)⟩ =>
+     fun h2 : ∀ x, p x =>
+     show r from hb (h2 b))
+    (fun h1 : (∀ x, p x) → r =>
+     show ∃ x, p x → r from
+       byCases
+         (fun hap : ∀ x, p x => ⟨a, λ h' => h1 hap⟩)
+         (fun hnap : ¬ ∀ x, p x =>
+          byContradiction
+            (fun hnex : ¬ ∃ x, p x → r =>
+              have hap : ∀ x, p x :=
+                fun x =>
+                byContradiction
+                  (fun hnp : ¬ p x =>
+                    have hex : ∃ x, p x → r := ⟨x, (fun hp => absurd hp hnp)⟩
+                    show False from hnex hex)
+              show False from hnap hap)))
+
+
+
+example : (∃ x, p x → r) → (∀ x, p x) → r := by
+  intro h1 h2
+  obtain ⟨x,h3⟩ := h1
+  have h4 := h2 x
+  have h5 := h3 h4
+  exact h5
+
+
+
+
+
+namespace Hidden
+inductive Nat where
+  | zero : Nat
+  | succ : Nat → Nat
+  deriving Repr
+
+#check Nat.rec
+
+def add (m n : Nat) : Nat :=
+  match n with
+  | Nat.zero   => m
+  | Nat.succ n => Nat.succ (add m n)
+
+open Nat
+
+#eval add (succ (succ zero)) (succ zero)
+
+
+
+namespace Nat
+
+def add (m n : Nat) : Nat :=
+  match n with
+  | Nat.zero   => m
+  | Nat.succ n => Nat.succ (add m n)
+
+instance : Add Nat where
+  add := add
+
+theorem add_zero (m : Nat) : m + zero = m := rfl
+theorem add_succ (m n : Nat) : m + succ n = succ (m + n) := rfl
+
+end Nat
+end Hidden
+
+
+namespace Nat
+theorem zero_add2 (n : Nat) : 0 + n = n :=
+  Nat.recOn (motive := fun x => 0 + x = x)
+   n
+   (show 0 + 0 = 0 from rfl)
+   (fun (n : Nat) (ih : 0 + n = n) =>
+    show 0 + succ n = succ n from
+    calc 0 + succ n
+      _ = succ (0 + n) := rfl
+      _ = succ n       := by rw [ih])
+
+
+theorem add_assoc2 (m n k : Nat) : m + n + k = m + (n + k) :=
+  Nat.recOn (motive := fun k => m + n + k = m + (n + k)) k
+    (show m + n + 0 = m + (n + 0) from rfl)
+    (fun k (ih : m + n + k = m + (n + k)) =>
+      show m + n + succ k = m + (n + succ k) from
+      calc m + n + succ k
+        _ = succ (m + n + k)   := rfl
+        _ = succ (m + (n + k)) := by rw [ih]
+        _ = m + succ (n + k)   := rfl
+        _ = m + (n + succ k)   := rfl)
+
+#print add_assoc2
+end Nat
+
+
+inductive Weekday where
+ | sunday : Weekday
+ | monday : Weekday
+ | tuesday : Weekday
+ | wednesday : Weekday
+ | thursday : Weekday
+ | friday : Weekday
+ | saturday : Weekday
+ deriving Repr
+
+#check Weekday.sunday
+#check Weekday.monday
+
+open Weekday
+
+#check sunday
+#check monday
+
+
+def numberOfDay (d : Weekday) : Nat :=
+  match d with
+  | sunday    => 1
+  | monday    => 2
+  | tuesday   => 3
+  | wednesday => 4
+  | thursday  => 5
+  | friday    => 6
+  | saturday  => 7
+
+#eval numberOfDay Weekday.sunday  -- 1
+#eval numberOfDay Weekday.monday  -- 2
+#eval numberOfDay Weekday.tuesday -- 3
+
+namespace Weekday
+def next (d : Weekday) : Weekday :=
+  match d with
+  | sunday    => monday
+  | monday    => tuesday
+  | tuesday   => wednesday
+  | wednesday => thursday
+  | thursday  => friday
+  | friday    => saturday
+  | saturday  => sunday
+
+def previous (d : Weekday) : Weekday :=
+  match d with
+  | sunday    => saturday
+  | monday    => sunday
+  | tuesday   => monday
+  | wednesday => tuesday
+  | thursday  => wednesday
+  | friday    => thursday
+  | saturday  => friday
+
+#eval next (next tuesday)      -- Weekday.thursday
+#eval next (previous tuesday)  -- Weekday.tuesday
+
+example : next (previous tuesday) = tuesday :=
+  rfl
+
+end Weekday
+
+
+def next_previous (d : Weekday) : next (previous d) = d :=
+  match d with
+  | sunday    => rfl
+  | monday    => rfl
+  | tuesday   => rfl
+  | wednesday => rfl
+  | thursday  => rfl
+  | friday    => rfl
+  | saturday  => rfl
+
+
+
+theorem two_le {m : ℕ} (h0 : m ≠ 0) (h1 : m ≠ 1) : 2 ≤ m := by
+  cases m; contradiction
+  case succ m =>
+    cases m; contradiction
+    repeat apply Nat.succ_le_succ
+    apply zero_le
+
+theorem exists_prime_factor {n : Nat} (h : 2 ≤ n) : ∃ p : Nat, p.Prime ∧ p ∣ n := by
+  by_cases np : n.Prime
+  · use n, np
+  induction' n using Nat.strong_induction_on with n ih
+  rw [Nat.prime_def_lt] at np
+  push_neg at np
+  rcases np h with ⟨m, mltn, mdvdn, mne1⟩
+  have : m ≠ 0 := by
+    intro mz
+    rw [mz, zero_dvd_iff] at mdvdn
+    linarith
+  have mgt2 : 2 ≤ m := two_le this mne1
+  by_cases mp : m.Prime
+  · use m, mp
+  · rcases ih m mltn mgt2 mp with ⟨p, pp, pdvd⟩
+    use p, pp
+    apply pdvd.trans mdvdn
+
+#print exists_prime_factor
